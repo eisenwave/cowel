83459af0decca83d350200c0301a192f258019b1
Eisenwave <me@eisenwave.net>
2025-03-12 16:30:17 +0100

    add more typo search tests

--- src/test/cpp/test_typo.cpp
+++ src/test/cpp/test_typo.cpp
@@ -23,5 +23,29 @@ TEST(Typo, empty)
     EXPECT_EQ(expected, actual);
 }
 
+TEST(Typo, exact_match)
+{
+    constexpr std::u8string_view haystack[] { u8"abc", u8"12345", u8"xyz" };
+    constexpr std::u8string_view needle = u8"12345";
+
+    std::pmr::monotonic_buffer_resource memory;
+
+    constexpr Typo_Result expected { .index = 1, .distance = 0 };
+    const Typo_Result actual = closest_match(haystack, needle, &memory);
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(Typo, fuzzy_match)
+{
+    constexpr std::u8string_view haystack[] { u8"abc", u8"12345", u8"xyz" };
+    constexpr std::u8string_view needle = u8"1234";
+
+    std::pmr::monotonic_buffer_resource memory;
+
+    constexpr Typo_Result expected { .index = 1, .distance = 1 };
+    const Typo_Result actual = closest_match(haystack, needle, &memory);
+    EXPECT_EQ(expected, actual);
+}
+
 } // namespace
 } // namespace mmml
