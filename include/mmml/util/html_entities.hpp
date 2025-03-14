10b825c5422dca052bb26b0d0067bcc257770272
Eisenwave <me@eisenwave.net>
2025-03-14 17:52:34 +0100

    add HTML entity by name lookup

---
+++ include/mmml/util/html_entities.hpp
@@ -0,0 +1,29 @@
+#ifndef MMML_HTML_ENTITIES_HPP
+#define MMML_HTML_ENTITIES_HPP
+
+#include <array>
+#include <string_view>
+
+namespace mmml {
+
+/// @brief A lexicographically ordered array of HTML character names such as `u8"amp"`.
+extern const std::array<std::u8string_view, 2231> html_character_names;
+
+/// @brief Returns one or two code points given the name of an HTML charater reference.
+/// See https://html.spec.whatwg.org/dev/named-characters.html#named-character-references.
+///
+/// If the name isn't a known named character reference, returns `{U+0000, U+0000}`.
+/// If the name only corresponds to a single code point,
+/// the second element in the array is `U+0000`.
+///
+/// For example, given `"amp"`, returns `{U+0026, U+0000}` (`&`),
+/// and given `"caps"`, returns `{U+2229, U+FE00}` (`∩︀`)
+[[nodiscard]]
+std::array<char32_t, 2> code_points_by_character_reference_name(std::u8string_view name) noexcept;
+
+[[nodiscard]]
+std::u32string_view string_by_character_reference_name(std::u8string_view name) noexcept;
+
+} // namespace mmml
+
+#endif
