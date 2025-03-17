8dcb4c0167fa1a6ee08f651079dca8123f837241
Eisenwave <me@eisenwave.net>
2025-03-17 05:44:55 +0100

    add transparent less/greater

--- include/mmml/util/transparent_comparison.hpp
+++ include/mmml/util/transparent_comparison.hpp
@@ -13,7 +13,7 @@ struct Basic_Transparent_String_View_Hash {
     using string_view_type = std::basic_string_view<Char>;
 
     [[nodiscard]]
-    std::size_t operator()(string_view_type v) const
+    std::size_t operator()(string_view_type v) const noexcept
     {
         return std::hash<string_view_type> {}(v); // NOLINT(misc-include-cleaner)
     }
@@ -26,9 +26,35 @@ struct Basic_Transparent_String_View_Equals {
     using string_view_type = std::basic_string_view<Char>;
 
     [[nodiscard]]
-    bool operator()(string_view_type x, string_view_type y) const
+    bool operator()(string_view_type x, string_view_type y) const noexcept
     {
         return x == y;
+    }
+};
+
+template <typename Char>
+struct Basic_Transparent_String_View_Greater {
+    using is_transparent = void;
+    using char_type = Char;
+    using string_view_type = std::basic_string_view<Char>;
+
+    [[nodiscard]]
+    bool operator()(string_view_type x, string_view_type y) const noexcept
+    {
+        return x > y;
+    }
+};
+
+template <typename Char>
+struct Basic_Transparent_String_View_Less {
+    using is_transparent = void;
+    using char_type = Char;
+    using string_view_type = std::basic_string_view<Char>;
+
+    [[nodiscard]]
+    bool operator()(string_view_type x, string_view_type y) const noexcept
+    {
+        return x < y;
     }
 };
 
