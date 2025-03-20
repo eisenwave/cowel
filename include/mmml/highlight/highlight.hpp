1f40d73a5c173a4cb1304f908ec5b495df7b79dd
Eisenwave <me@eisenwave.net>
2025-03-20 19:45:51 +0100

    add dispatch based on Code_Language

--- include/mmml/highlight/highlight.hpp
+++ include/mmml/highlight/highlight.hpp
@@ -7,8 +7,12 @@
 #include <vector>
 
 #include "mmml/util/assert.hpp"
+#include "mmml/util/result.hpp"
 
 #include "mmml/fwd.hpp"
+
+#include "mmml/highlight/code_language.hpp"
+#include "mmml/highlight/highlight_error.hpp"
 
 namespace mmml {
 
@@ -102,6 +106,31 @@ bool highlight_cpp(
     std::pmr::memory_resource* memory,
     const Highlight_Options& options = {}
 );
+
+inline Result<void, Syntax_Highlight_Error> highlight(
+    std::pmr::vector<Highlight_Span>& out,
+    std::u8string_view source,
+    Code_Language language,
+    std::pmr::memory_resource* memory,
+    const Highlight_Options& options = {}
+)
+{
+    constexpr auto to_result = [](bool success) -> Result<void, Syntax_Highlight_Error> {
+        if (success) {
+            return {};
+        }
+        return Syntax_Highlight_Error::bad_code;
+    };
+
+    switch (language) {
+    case Code_Language::cpp: //
+        return to_result(highlight_cpp(out, source, memory, options));
+    case Code_Language::mmml: //
+        return to_result(highlight_mmml(out, source, memory, options));
+    default: //
+        return Syntax_Highlight_Error::unsupported_language;
+    }
+}
 
 } // namespace mmml
 
