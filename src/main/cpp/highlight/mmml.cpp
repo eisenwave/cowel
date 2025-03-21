257d0e1cfc1a917c1bd2f83295b0ef3e0f616847
Eisenwave <me@eisenwave.net>
2025-03-21 21:06:28 +0100

    highlight MMML comments correctly

--- src/main/cpp/highlight/mmml.cpp
+++ src/main/cpp/highlight/mmml.cpp
@@ -36,6 +36,7 @@ void highlight_mmml( //
 {
     std::size_t index = 0;
     const auto emit = [&](std::size_t length, Highlight_Type type) {
+        MMML_DEBUG_ASSERT(length != 0);
         const bool coalesce = options.coalescing && !out.empty() && out.back().value == type
             && out.back().end() == index;
         if (coalesce) {
@@ -47,57 +48,114 @@ void highlight_mmml( //
         index += length;
     };
 
-    [[maybe_unused]]
-    std::size_t in_comment
-        = 0;
+    // Indicates how deep we are in a comment.
+    //   0: Not in a comment.
+    //   1: Within the directive name, arguments, etc. but not in the comment block.
+    // > 1: In the comment block.
+    std::size_t in_comment = 0;
+    std::size_t comment_delimiter_length = 0;
+    std::size_t comment_content_length = 0;
+
     for (const auto& i : instructions) {
-        switch (i.type) {
-            using enum AST_Instruction_Type;
-        case skip: //
-            index += i.n;
-            break;
-        case escape: //
-            emit(i.n, Highlight_Type::string_escape);
-            break;
-        case text: //
-            index += i.n;
-            break;
-        case argument_name: //
-            emit(i.n, Highlight_Type::attribute);
-            break;
-        case push_directive: {
-            const std::u8string_view directive_name = source.substr(index, i.n);
-            // TODO: highlight comment contents specially,
-            //       perhaps by recursing into another function that handles comments
-            if (directive_name == u8"\\comment" || directive_name == u8"\\-comment") {
-                emit(i.n, Highlight_Type::comment_delimiter);
-                ++in_comment;
+        using enum AST_Instruction_Type;
+        if (in_comment != 0) {
+            std::size_t& target
+                = in_comment > 1 ? comment_content_length : comment_delimiter_length;
+            switch (i.type) {
+            case skip:
+            case escape:
+            case text:
+            case argument_name:
+            case push_directive: {
+                target += i.n;
+                break;
             }
-            else {
-                emit(i.n, Highlight_Type::tag);
+            case pop_directive: {
+                if (in_comment == 1) {
+                    in_comment = 0;
+                }
+                break;
             }
-            break;
-        }
-        case pop_directive: {
-            --in_comment;
-            break;
-        }
+            case argument_equal:
+            case argument_comma:
+            case push_arguments:
+            case pop_arguments: {
+                ++target;
+                break;
+            }
 
-        case argument_equal: // =
-        case argument_comma: // ,
-            emit(1, Highlight_Type::symbol);
-            break;
-        case push_arguments: // [
-        case pop_arguments: // ]
-        case push_block: // {
-        case pop_block: // }
-            emit(i.n, Highlight_Type::symbol_important);
-            break;
+            case push_document:
+            case pop_document:
+            case push_argument:
+            case pop_argument: break;
 
-        case push_document:
-        case pop_document:
-        case push_argument:
-        case pop_argument: break;
+            case push_block: {
+                ++target;
+                if (in_comment++ <= 1) {
+                    emit(comment_delimiter_length, Highlight_Type::comment_delimiter);
+                }
+                break;
+            }
+            case pop_block: {
+                if (--in_comment == 1) {
+                    if (comment_content_length != 0) {
+                        emit(comment_content_length, Highlight_Type::comment);
+                    }
+                    emit(1, Highlight_Type::comment_delimiter);
+                }
+                else {
+                    ++comment_content_length;
+                }
+                break;
+            }
+            }
+        }
+        else {
+            switch (i.type) {
+            case skip: //
+                index += i.n;
+                break;
+            case escape: //
+                emit(i.n, Highlight_Type::string_escape);
+                break;
+            case text: //
+                index += i.n;
+                break;
+            case argument_name: //
+                emit(i.n, Highlight_Type::attribute);
+                break;
+            case push_directive: {
+                const std::u8string_view directive_name = source.substr(index, i.n);
+                // TODO: highlight comment contents specially,
+                //       perhaps by recursing into another function that handles comments
+                if (directive_name == u8"\\comment" || directive_name == u8"\\-comment") {
+                    in_comment = 1;
+                    comment_delimiter_length = i.n;
+                    comment_content_length = 0;
+                }
+                else {
+                    emit(i.n, Highlight_Type::tag);
+                }
+                break;
+            }
+
+            case argument_equal: // =
+            case argument_comma: // ,
+                emit(1, Highlight_Type::symbol);
+                break;
+            case push_arguments: // [
+            case pop_arguments: // ]
+            case push_block: // {
+            case pop_block: // }
+                emit(1, Highlight_Type::symbol_important);
+                break;
+
+            case push_document:
+            case pop_document:
+            case push_argument:
+            case pop_argument:
+            case pop_directive: break;
+            }
         }
     }
 }
