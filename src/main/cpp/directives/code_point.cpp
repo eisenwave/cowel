67cca85721312384149c5a566cdc2219e55f0585
Eisenwave <me@eisenwave.net>
2025-04-20 17:58:32 +0200

    clean up some utilities

--- src/main/cpp/directives/code_point.cpp
+++ src/main/cpp/directives/code_point.cpp
@@ -15,17 +15,16 @@ char32_t get_code_point(const ast::Directive& d, Context& context)
 {
     if (!d.get_arguments().empty()) {
         const Source_Span pos = d.get_arguments().front().get_source_span();
-        context.try_emit(
-            Severity::warning, diagnostic::U_args_ignored, pos,
-            u8"Arguments to this directive are ignored."
+        context.try_warning(
+            diagnostic::U_args_ignored, pos, u8"Arguments to this directive are ignored."
         );
     }
     std::pmr::vector<char8_t> data { context.get_transient_memory() };
     to_plaintext(data, d.get_content(), context);
     const std::u8string_view digits = trim_ascii_blank({ data.data(), data.size() });
     if (digits.empty()) {
-        context.try_emit(
-            Severity::error, diagnostic::U_blank, d.get_source_span(),
+        context.try_error(
+            diagnostic::U_blank, d.get_source_span(),
             u8"Expected a sequence of hexadecimal digits, but got a blank string."
         );
         return error_point;
@@ -33,8 +32,8 @@ char32_t get_code_point(const ast::Directive& d, Context& context)
 
     std::optional<std::uint32_t> value = from_chars<std::uint32_t>(digits, 16);
     if (!value) {
-        context.try_emit(
-            Severity::error, diagnostic::U_digits, d.get_source_span(),
+        context.try_error(
+            diagnostic::U_digits, d.get_source_span(),
             u8"Expected a sequence of hexadecimal digits."
         );
         return error_point;
@@ -42,8 +41,8 @@ char32_t get_code_point(const ast::Directive& d, Context& context)
 
     const auto code_point = char32_t(*value);
     if (!is_scalar_value(code_point)) {
-        context.try_emit(
-            Severity::error, diagnostic::U_nonscalar, d.get_source_span(),
+        context.try_error(
+            diagnostic::U_nonscalar, d.get_source_span(),
             u8"The given hex sequence is not a Unicode scalar value. "
             u8"Therefore, it cannot be encoded as UTF-8."
         );
