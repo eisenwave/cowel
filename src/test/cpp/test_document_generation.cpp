817786df7ce2130e3f660007964987aa6e0c06e0
Eisenwave <me@eisenwave.net>
2025-04-24 11:15:45 +0200

    fix bad test expectations

--- src/test/cpp/test_document_generation.cpp
+++ src/test/cpp/test_document_generation.cpp
@@ -228,7 +228,7 @@ constexpr Basic_Test basic_tests[] {
     { Source { u8"\\h3[id=user id]{Heading}\n" },
       Source { u8"<h3 id=\"user id\"><a class=para href=\"#user id\"></a>Heading</h3>\n" } },
     { Source { u8"\\h4[id=user-id]{Heading}\n" },
-      Source { u8"<h3 id=user-id><a class=para href=#user-id></a>Heading</h3>\n" } },
+      Source { u8"<h4 id=user-id><a class=para href=#user-id></a>Heading</h4>\n" } },
 
     { Source { u8"\\code{}\n" },
       Source { u8"<code></code>\n" },
