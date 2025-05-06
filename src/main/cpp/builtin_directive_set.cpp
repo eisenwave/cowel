e91db66fb6d4c5ee482b8cb5998ca8470857f1d1
Eisenwave <me@eisenwave.net>
2025-05-06 12:24:10 +0200

    add \grammar directive

--- src/main/cpp/builtin_directive_set.cpp
+++ src/main/cpp/builtin_directive_set.cpp
@@ -60,6 +60,8 @@ struct Builtin_Directive_Set::Impl {
         {};
     Special_Block_Behavior example //
         { u8"example-block" };
+    In_Tag_Behavior grammar //
+        { u8"dl", u8"grammar", Directive_Display::block };
     Fixed_Name_Passthrough_Behavior gterm //
         { u8"g-term", Directive_Category::formatting, Directive_Display::in_line };
     Heading_Behavior h1 //
@@ -217,6 +219,7 @@ Distant<std::u8string_view> Builtin_Directive_Set::fuzzy_lookup_name(
         u8"-em",
         u8"-error",
         u8"-example",
+        u8"-grammar",
         u8"-gterm",
         u8"-h1",
         u8"-h2",
@@ -364,6 +367,8 @@ Directive_Behavior* Builtin_Directive_Set::operator()(std::u8string_view name) c
         break;
 
     case u8'g':
+        if (name == u8"grammar")
+            return &m_impl->grammar;
         if (name == u8"gterm")
             return &m_impl->gterm;
         break;
