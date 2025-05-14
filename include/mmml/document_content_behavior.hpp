7a56820e795fe1c3ea0b2cfcfdaebac4b1565c0f
Eisenwave <me@eisenwave.net>
2025-05-14 18:31:39 +0200

    store macro behavior in Document_Content_Behavior

--- include/mmml/document_content_behavior.hpp
+++ include/mmml/document_content_behavior.hpp
@@ -12,14 +12,13 @@
 namespace mmml {
 
 struct [[nodiscard]] Head_Body_Content_Behavior : Content_Behavior {
-
     void generate_plaintext(std::pmr::vector<char8_t>&, std::span<const ast::Content>, Context&)
         const final
     {
         MMML_ASSERT_UNREACHABLE(u8"Unimplemented.");
     }
 
-    void generate_html(HTML_Writer& out, std::span<const ast::Content>, Context&) const final;
+    void generate_html(HTML_Writer& out, std::span<const ast::Content>, Context&) const override;
 
     virtual void generate_head(HTML_Writer& out, std::span<const ast::Content>, Context&) const = 0;
     virtual void generate_body(HTML_Writer& out, std::span<const ast::Content>, Context&) const = 0;
@@ -27,6 +26,16 @@ struct [[nodiscard]] Head_Body_Content_Behavior : Content_Behavior {
 
 struct [[nodiscard]]
 Document_Content_Behavior final : Head_Body_Content_Behavior {
+private:
+    Directive_Behavior& m_macro_behavior;
+
+public:
+    constexpr explicit Document_Content_Behavior(Directive_Behavior& macro_behavior)
+        : m_macro_behavior { macro_behavior }
+    {
+    }
+
+    void generate_html(HTML_Writer& out, std::span<const ast::Content>, Context&) const final;
     void generate_head(HTML_Writer& out, std::span<const ast::Content>, Context&) const final;
     void generate_body(HTML_Writer& out, std::span<const ast::Content>, Context&) const final;
 };
