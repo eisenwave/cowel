f074a75206a63c33ccbf90deef133969f1935d50
Eisenwave <me@eisenwave.net>
2025-06-11 15:09:47 +0200

    import decode_and_length_or_replacement

--- include/cowel/util/unicode.hpp
+++ include/cowel/util/unicode.hpp
@@ -13,7 +13,7 @@ using ulight::utf8::code_points_unchecked;
 using ulight::utf8::Code_Units_And_Length;
 using ulight::utf8::decode;
 using ulight::utf8::decode_and_length;
-using ulight::utf8::decode_and_length_or_throw;
+using ulight::utf8::decode_and_length_or_replacement;
 using ulight::utf8::decode_and_length_unchecked;
 using ulight::utf8::decode_unchecked;
 using ulight::utf8::encode8_unchecked;
