158998307ded4cce766aaeaacfca1e2ed0b3ebaa
Eisenwave <me@eisenwave.net>
2025-07-17 15:00:21 +0200

    fix duplicate/incompatible declarations compared to ulight

--- include/cowel/util/meta.hpp
+++ include/cowel/util/meta.hpp
@@ -4,48 +4,19 @@
 #include <concepts>
 #include <type_traits>
 
+#include "ulight/const.hpp"
+
 namespace cowel {
 
-/// @brief If `C` is true, alias for `const T`, otherwise for `T`.
-template <typename T, bool C>
-using const_if_t = std::conditional_t<C, const T, T>;
-
-template <typename T, bool C>
-struct Follow_Ref_Const_If {
-    using type = const_if_t<T, C>;
-};
-
-template <typename T, bool C>
-struct Follow_Ref_Const_If<T&, C> {
-    using type = const_if_t<T, C>&;
-};
-
-template <typename T, bool C>
-struct Follow_Ref_Const_If<T&&, C> {
-    using type = const_if_t<T, C>&&;
-};
-
-/// @brief Like `const_if_t`, but if `T` is a reference,
-/// `const` is conditionally added to the referenced type.
-///
-/// For example, `follow_ref_const_if_t<int&, true>` is `const int&`.
-template <typename T, bool C>
-using follow_ref_const_if_t = Follow_Ref_Const_If<T, C>::type;
-
-/// @brief If `std::is_const_v<U>`, alias for `const T`, otherwise for `T`.
-template <typename T, typename U>
-using const_like_t = const_if_t<T, std::is_const_v<U>>;
+using ulight::const_if_t;
+using ulight::const_like_t;
+using ulight::const_v;
+using ulight::Constant;
+using ulight::Follow_Ref_Const_If;
+using ulight::follow_ref_const_if_t;
 
 template <typename>
 inline constexpr bool dependent_false = false;
-
-template <auto X>
-struct Constant {
-    static constexpr decltype(X) value = X;
-};
-
-template <auto X>
-inline constexpr Constant<X> constant_v {};
 
 template <typename T>
 concept trivial = std::is_trivially_copyable_v<T> && std::is_trivially_default_constructible_v<T>;
