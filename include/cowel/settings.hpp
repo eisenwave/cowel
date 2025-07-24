f6b4ca4baf3bf08c1399588e5fccc0f9f0781e3d
Eisenwave <me@eisenwave.net>
2025-07-24 17:10:30 +0200

    add default_char_sequence_buffer_size setting

--- include/cowel/settings.hpp
+++ include/cowel/settings.hpp
@@ -1,6 +1,8 @@
 #ifndef COWEL_SETTINGS_HPP
 #define COWEL_SETTINGS_HPP
 
+#include <cstddef>
+
 namespace cowel {
 
 /// @brief If `true`, adds assertions in various places
@@ -8,6 +10,10 @@ namespace cowel {
 /// The point is to identify potential optimization opportunities/correctness problems,
 /// where empty strings ultimately have no effect anyway.
 inline constexpr bool enable_empty_string_assertions = false;
+
+/// @brief The default `char8_t` buffer size
+/// when it is necessary to process a `Char_Sequence` in a chunked/buffered way.
+inline constexpr std::size_t default_char_sequence_buffer_size = 1024;
 
 } // namespace cowel
 
