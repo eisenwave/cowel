7a28d8236306d31fc6916ea7c02c7f60561286e7
Eisenwave <me@eisenwave.net>
2025-08-01 23:06:06 +0200

    strip devDependencies for released npm package

---
+++ src/util/transform-package-json.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+import { readFile, writeFile } from "fs/promises";
+import { argv } from "process";
+
+if (argv.length < 4) {
+    console.error(`Usage: node ${argv[1]} <input.json> <output.json>`);
+    process.exit(1);
+}
+
+const [inputPath, outputPath] = argv.slice(2);
+
+const content = await readFile(inputPath, "utf-8");
+const json = JSON.parse(content);
+
+delete json.devDependencies;
+
+await writeFile(outputPath, JSON.stringify(json, null, 2) + "\n");
