b1ea7e95cd66cb4c717fd83a586e7609575906d5
Eisenwave <me@eisenwave.net>
2026-01-25 10:35:44 +0100

    install Boost automatically in CMakeLists.txt

--- src/util/boost-install.sh
+++ src/util/boost-install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-DIRECTORY="third_party/boost"
-git clone https://github.com/boostorg/boost.git "$DIRECTORY"
+
+if [[ $# -eq 0 ]]; then
+  echo "Error: Boost installation argument required (usually third_party/boost)"
+  echo "Usage: $0 <directory>"
+  exit 1
+fi
+
+BOOST_URL="https://github.com/boostorg/boost.git"
+DIRECTORY="$1"
+
+git clone "$BOOST_URL" "$DIRECTORY"
 (
   cd "$DIRECTORY" || exit 1
   git submodule update --init \
